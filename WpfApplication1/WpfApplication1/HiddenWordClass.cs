f6738b854d1e9cab7f5d8a6fea223c62a4847141
Cyril <cyril.rat@gmail.com>
2017-02-19 03:50:37 +0100

    First part App Working

---
+++ WpfApplication1/WpfApplication1/HiddenWordClass.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class HiddenWordClass
+    {
+        private String word;
+        private Int32 wordLength;
+        private String hiddenWord;
+
+        public HiddenWordClass(String word)
+        {
+            Word = word;
+        }
+
+        //Check whether the letter is in the word to find
+        public bool isLetterPresent (char c)
+        {
+            bool b = false;
+            
+            StringBuilder stringBuilder = new StringBuilder(HiddenWord);
+
+            for (int i=0; i<wordLength; i++)
+            {
+                if (Word[i] == c && HiddenWord[i] != c)
+                {
+                    stringBuilder[i] = c;
+                    b = true;
+                }
+            }
+
+            HiddenWord = stringBuilder.ToString();
+            
+            //Return true if letter is in the word
+            return b;
+        }
+
+        //Check whether the word has been completely found
+        public bool isFinish()
+        {
+            if (Word == HiddenWord) return true;
+            else return false;
+        }
+
+        //Hide the word and return the String hiddenWord with * to display
+        public String hideWord()
+        {
+            if (Word != null)
+            {
+                wordLength = Word.Length;
+                for (int i=0; i<wordLength; i++)
+                {
+                    HiddenWord += "*";
+                }
+                return HiddenWord;
+            }
+            else return null;
+            
+        }
+
+        //Reset the word to null
+        public void reset()
+        {
+            Word = null;
+        }
+
+
+        public string HiddenWord
+        {
+            get
+            {
+                return hiddenWord;
+            }
+
+            set
+            {
+                hiddenWord = value;
+            }
+        }
+
+        public string Word
+        {
+            get
+            {
+                return word;
+            }
+            set
+            {
+                word = value;
+            }
+        }
+    }
+}
--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -21,51 +21,138 @@ namespace WpfApplication1
     public partial class MainWindow : Window
     {
 
-        private String hiddenWord;
-        private Int32 hiddenWordLength;
-        public static readonly String hideButtonText = "Hide";
-        public static readonly String resetButtonText = "Reset";
-
+        private readonly String HIDE_BUTTON_TEXT = "Hide";
+        private readonly String RESET_BUTTON_TEXT = "Reset";
+        private HiddenWordClass hiddenWordClass;
+        private Int32 MIN_WORD_LENGTH = 4;
+        private readonly Int32 MAX_CHANCES = 5;
+        private Int32 chanceCount;
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void hideButton_Click(object sender, RoutedEventArgs e)
         {
-            if ((String)hideButton.Content == hideButtonText)
+            if ((String)hideButton.Content == HIDE_BUTTON_TEXT)
             {
-                //set the hidden word and its length
-                hiddenWord = hiddenWordTextBox.Text;
-                hiddenWordLength = hiddenWord.Length;
-
-                //Clear the textbox and disable it
-                hiddenWordTextBox.Clear();
-                hiddenWordTextBox.IsEnabled = false;
-                
-                //Set the button context to Reset
-                hideButton.Content = resetButtonText;
-                return;
+                String s = hiddenWordTextBox.Text;
+                if (s.Length <= MIN_WORD_LENGTH)
+                {
+                    MessageBox.Show("Please enter more 4 letters !", "Error letters Numbers", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                else {
+                    start();
+                    return;
+                }
             }
 
-            if ((String)hideButton.Content == resetButtonText)
+            if ((String)hideButton.Content == RESET_BUTTON_TEXT)
             {
-                //set the hidden word and its length
-                hiddenWord = null;
-                hiddenWordLength = 0;
-
-                //Clear the textbox and disable it
-                hiddenWordTextBox.Clear();
-                hiddenWordTextBox.IsEnabled = true;
-
-                //Set the button context to Hide
-                hideButton.Content = hideButtonText;
+                reset();
                 return;
-
             }
+        }
 
+        private void reset()
+        {
+            hiddenWordClass.reset();
 
+            //Clear the textbox and disable it
+            hiddenWordTextBox.Clear();
+            hiddenWordTextBox.IsEnabled = true;
+
+            letterTextBox.Clear();
+
+            chanceLabel.Content = "";
+
+            chanceCount = MAX_CHANCES;
+            hideControl();
+
+            //Set the button context to Hide
+            hideButton.Content = HIDE_BUTTON_TEXT;
+        }
+
+        private void start()
+        {
+            hiddenWordClass = new HiddenWordClass(hiddenWordTextBox.Text);
+
+            chanceCount = MAX_CHANCES;
+
+            chanceLabel.Content = chanceCount;
+
+            //Clear the textbox and disable it
+            hiddenWordTextBox.Clear();
+            hiddenWordTextBox.Text = hiddenWordClass.hideWord();
+            hiddenWordTextBox.IsEnabled = false;
+
+            showControl();
+
+            //Set the button context to Reset
+            hideButton.Content = RESET_BUTTON_TEXT;
+        }
+
+        private void showControl()
+        {
+            okButton.Visibility = Visibility.Visible;
+            letterTextBox.Visibility = Visibility.Visible;
+            chanceLabel.Visibility = Visibility.Visible;
+            chanceLabelText.Visibility = Visibility.Visible;
+        }
+
+        private void hideControl()
+        {
+            okButton.Visibility = Visibility.Hidden;
+            letterTextBox.Visibility = Visibility.Hidden;
+            chanceLabel.Visibility = Visibility.Hidden;
+            chanceLabelText.Visibility = Visibility.Hidden;
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (letterTextBox.Text.Length != 0) {
+                String s = letterTextBox.Text;
+                char c = s[0];
+                if (hiddenWordClass.isLetterPresent(c))
+                {
+                    hiddenWordTextBox.Clear();
+                    hiddenWordTextBox.Text = hiddenWordClass.HiddenWord;
+                }
+                else
+                {
+                    chanceCount--;
+                    chanceLabel.Content = chanceCount;
+                    if(chanceCount == 0)
+                    {
+                        gameOver();
+                    }
+                }
+
+                letterTextBox.Clear();
+
+                if (hiddenWordClass.isFinish())
+                {
+                    gameFinish();
+                }
+            }
+        }
+
+        private void gameFinish()
+        {
+            MessageBox.Show("Well Done !\nYou Won", "Finished", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            reset();
 
         }
+
+        private void gameOver()
+        {
+            MessageBox.Show("Game Over :( !\n The word was : " + hiddenWordClass.Word
+                , "Game Over", MessageBoxButton.OK, MessageBoxImage.Warning);
+            reset();
+        }
+
     }
 }
