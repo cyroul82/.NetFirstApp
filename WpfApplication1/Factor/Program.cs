3ba4b3d098fbe2ba0b62f29f4688f165cf6c7da7
Cyril RAT <cyril.rat@gmail.com>
2017-02-20 16:25:00 +0100

    Added Factor App Exo Afpa !

---
+++ WpfApplication1/Factor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String cRep;
+            Int32 nFactor;
+            Int32 nNombre;
+
+            cRep = "o";
+            while(cRep == "o" || cRep == "O")
+            {
+                nFactor = 1;
+                Console.WriteLine("Calcul de factorielle");
+                Console.Write("Entrez un nombre: ");
+                nNombre = Convert.ToInt32(Console.ReadLine());
+                
+                for (int i=1; i<= nNombre; i++)
+                {
+                    
+                    nFactor = nFactor * i;
+                }
+                Console.WriteLine("La factorielle de {0} est donc : {1}", nNombre, nFactor);
+
+                Console.WriteLine("Voulez-vous remcommence (o/n)" );
+                cRep = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bye bye");
+        }
+    }
+}
