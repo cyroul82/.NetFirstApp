0942ea5dd465fc085db4e9d4f37b6ebde0936809
Cyril RAT <cyril.rat@gmail.com>
2017-02-21 08:52:14 +0100

    Hypo App: Added method to handle the TryParse and Added a loop to repeat the app !!!

--- WpfApplication1/Hypo/Program.cs
+++ WpfApplication1/Hypo/Program.cs
@@ -10,37 +10,45 @@ namespace Hypo
     {
         static void Main(string[] args)
         {
-            Double firstSide;
-            Double secondSide;
-            Double hypothenus;
+            Boolean flag = true;
+            while (flag)
+            {
+                Console.Write("Length of the first side  : ");
+                String s = Console.ReadLine();
+                Double firstSide = getDouble(s);
 
-            Console.Write("Length of the first side  : ");
-            String s = Console.ReadLine();
+                Console.Write("Length of the Second side  : ");
+                s = Console.ReadLine();
+                Double secondSide = getDouble(s);
 
-            while (!Double.TryParse(s, out firstSide))
+                Console.WriteLine("Hypothenus = " + getHypothenus(firstSide, secondSide));
+
+                Console.WriteLine("Another Calcul :(y/n)");
+                String yesOrNo = Console.ReadLine();
+                yesOrNo.ToLower();
+                flag = (yesOrNo == "y") ? true : false;
+            }
+            
+            
+        
+        }
+
+        //Convert a string in Double, wait until true !
+        private static Double getDouble(String s)
+        {
+            Double d;
+            while (!Double.TryParse(s, out d))
             {
                 Console.WriteLine("the value isn't a double ");
                 Console.Write("Please enter a new number : ");
                 s = Console.ReadLine();
             }
+            return d;
+        }
 
-            Console.Write("Length of the Second side  : ");
-            String s1 = Console.ReadLine();
-
-            while (!Double.TryParse(s1, out secondSide))
-            {
-                Console.WriteLine("the value isn't a double ");
-                Console.Write("Please enter a new number : ");
-                s1 = Console.ReadLine();
-            }
-        
-
-            hypothenus = Math.Sqrt(Math.Pow(firstSide, 2) + Math.Pow(secondSide, 2));
-
-            Console.WriteLine("Hypothenus = " + hypothenus);
-
-            Console.ReadLine();
-        
+        private static Double getHypothenus(Double firstSide, Double secondSide)
+        {
+            return Math.Sqrt(Math.Pow(firstSide, 2) + Math.Pow(secondSide, 2));
         }
     }
 }
